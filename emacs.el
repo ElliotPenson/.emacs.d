f7ec9bb85ba8826f3adc6882576c34fc917a998c
Elliot Penson <elliotpenson@gmail.com>
2015-05-01 16:55:47 -0400

    Include various packages and update file structure

--- emacs.el
+++ emacs.el
@@ -1,16 +1,21 @@
-;;;; emacs config file
-;;;; Elliot Penson
+;;;; Emacs configuration file 
+;;;; by Elliot Penson
+
+;;; Interface
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq inhibit-startup-message t)
 
 ;;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'base16-default t)
 
-;;; Backup Files
-(setq make-backup-files nil)
-
 ;;; Line Numbers
 (global-linum-mode t)
 (setq linum-format "%d ")
+
+;;; Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; MELPA
 (require 'package)
@@ -19,10 +24,38 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
 (package-initialize)
+
+(defvar my-packages '(better-defaults paredit projectile clojure-mode cider))
+
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;;; SLIME
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
 
-;(setq find-file-visit-truename t)
+;;; Org-Mode
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+;;; Paredit
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural-editing of Lisp code."
+  t)
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hood #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook #'enable-paredit-mode)
+
+;;; Paredit in SLIME
+(add-hook 'slime-repl-mode-hood (lambda () (paredit-mode +1)))
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (red-kdb-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook #'override-slime-repl-bindings-with-paredit)
