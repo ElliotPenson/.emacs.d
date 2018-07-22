422d6b9e934bed312d18275259c10df138abe8e0
Elliot Penson <elliot.penson@it.com>
2018-07-21 23:26:08 -0400

    Allow quick file renaming

--- emacs.el
+++ emacs.el
@@ -61,6 +61,26 @@
         (replace-match (number-to-string (1+ number-at-point))))
     (error "No number at point")))
 
+(defun rename-current-buffer-file ()
+  "Rename the current buffer and the file it is visiting. Taken from
+   whattheemacsd.com."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
 (push (cons "\\.cl$" 'lisp-mode)
       auto-mode-alist)
 
