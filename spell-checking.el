6f5ba92692ec62340f9522c48272f00a22e1c2dd
Elliot Penson <elliotpenson@gmail.com>
2020-08-10 20:36:36 -0700

    Fix a compiler warning by correctly branching in the spell check cond

--- spell-checking.el
+++ spell-checking.el
@@ -9,11 +9,11 @@
 
 (cond ((eq system-type 'darwin)
        (setq ispell-program-name
-             "/usr/local/Cellar/ispell/3.4.00/bin/ispell")
-       (eq system-type 'gnu/linux)
+             "/usr/local/Cellar/ispell/3.4.00/bin/ispell"))
+      ((eq system-type 'gnu/linux)
        (setq ispell-program-name
-             "/usr/bin/ispell")
-       (t (error "Unknown system type '%s'" system-type))))
+             "/usr/bin/ispell"))
+      (t (error "Unknown system type '%s'" system-type)))
 
 (dolist (hook '(text-mode-hook
                 org-mode-hook))
